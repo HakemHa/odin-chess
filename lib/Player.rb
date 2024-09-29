5e6a3af287c067ce5ca6e00ec3b70aa154f6a651
HakemHa <itaib2004@gmail.com>
2024-09-29 08:55:34 +0300

    Refactored Player
    Isolated handle_input and key_to_command functions
    Ammended: Validate now is default in case it is not provided

--- lib/Player.rb
+++ lib/Player.rb
@@ -8,46 +8,11 @@ class Player
 
   def play(coords, curr_index, moves)
     state = [coords, curr_index, moves]
-    input = nil
-    while input.nil? do
-      input = STDIN.getch
-      chars_in_queue = true
-      while chars_in_queue do
-        chars_in_queue = false
-        begin
-          input += Timeout::timeout(0.01) {
-            STDIN.getch
-          }
-        rescue
-          chars_in_queue = false
-        end
-      end
-      input = validate(input)
-    end
-    return act(input, state)
+    validateFunc = nil
+    actFunc = method(:act)
+    Player.handle_input(validateFunc, actFunc, *state)
   end
-
-  def validate(input)
-    key_to_command = {
-      "\e[A" => "+1", 
-      "\e[B" => "-1", 
-      "\e[C" => "+1", 
-      "\e[D" => "-1", 
-      " " => "submit", 
-      "\r" => "submit", 
-      "\u007F" => "reset", 
-      "e" => "exit", 
-      "q" => "exit",
-      "\u0003" => "hard_exit",
-    }
-    valid_keys = key_to_command.keys()
-    is_valid_key = valid_keys.include?(input)
-    if is_valid_key then
-      return key_to_command[input]
-    end
-    return nil
-  end
-
+  
   def act(input, state)
     coords, curr_index, moves = state[0], state[1], state[2]
     case input
@@ -74,5 +39,53 @@ class Player
     else
       return coords
     end
+  end
+  
+  def self.key_to_command
+    {
+      "\e[A" => "+1", 
+      "\e[B" => "-1", 
+      "\e[C" => "+1", 
+      "\e[D" => "-1", 
+      " " => "submit", 
+      "\r" => "submit", 
+      "\u007F" => "reset", 
+      "e" => "exit", 
+      "q" => "exit",
+      "\u0003" => "hard_exit",
+    }
+  end
+  
+  def self.handle_input(validate, act, *state)
+    if validate.nil? then
+      validate = method(:validate)
+    end
+    input = nil
+    while input.nil? do
+      input = STDIN.getch
+      chars_in_queue = true
+      while chars_in_queue do
+        chars_in_queue = false
+        begin
+          input += Timeout::timeout(0.01) {
+            STDIN.getch
+          }
+        rescue
+          chars_in_queue = false
+        end
+      end
+      input = validate.call(input)
+    end
+    return act.call(input, state)
+  end
+
+  def self.validate(input)
+    key_to_command = Player.key_to_command
+    valid_keys = key_to_command.keys()
+    is_valid_key = valid_keys.include?(input)
+    if is_valid_key then
+      return key_to_command[input]
+    end
+    return nil
   end
 end
